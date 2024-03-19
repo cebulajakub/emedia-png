110f550a7b40446eb75837230a2aac3d9142397d
Finnal project <259657@student.pwr.edu.pl>
2024-03-19 13:35:34 +0100

    plot

--- Emedia.py
+++ Emedia.py
@@ -238,6 +238,64 @@ def furier_trans_png(file_path):
     plt.show()
 
 
+def low_pass_filter(img_gray,cutoff_freq,spectrum):
+    #maska
+    rows, cols = img_gray.shape
+    crow, ccol = rows // 2, cols // 2
+    mask = np.zeros((rows, cols), np.uint8)
+    mask[crow - cutoff_freq:crow + cutoff_freq, ccol - cutoff_freq:ccol + cutoff_freq] = 1
+
+    #filtracja
+    filtered_spectrum = spectrum * mask
+
+    # odwrotna
+    filtered_img = np.fft.ifft2(np.fft.ifftshift(filtered_spectrum)).real
+    return filtered_img
+def furier_trans_pngg(file_path, cutoff_freq):
+    # Wczytaj obraz
+    img = plt.imread(file_path)
+
+    # Przekształć obraz do skali szarości
+    img_gray = np.mean(img, axis=-1)
+
+    # Normalizuj obraz do przedziału [0, 1]
+    img_normalized = (img_gray - img_gray.min()) / (img_gray.max() - img_gray.min() + 1e-10)
+
+    # Oblicz transformatę Fouriera
+    spectrum = np.fft.fftshift(np.fft.fft2(img_normalized))
+
+    low_pass = low_pass_filter(img_gray, cutoff_freq, spectrum)
+
+    # Wyświetl obraz przefiltrowany
+    plt.figure(figsize=(12, 12))
+
+    plt.subplot(3, 2, 1)
+    plt.imshow(img, cmap='gray')
+    plt.title('Color Image')
+
+    plt.subplot(3, 2, 2)
+    plt.imshow(img_gray, cmap='gray')
+    plt.title('Grayscale Image')
+
+    plt.subplot(3, 2, 3)
+    spectrum_log = np.abs(spectrum) + 1e-10
+    plt.imshow(np.log(spectrum_log), cmap='gray')
+    plt.title('Fourier Transform Spectrum')
+    plt.colorbar()
+
+    plt.subplot(3, 2, 4)
+    edges = plot_edges(img_normalized)
+    plt.imshow(edges, cmap='gray')
+    plt.title('Edges')
+    plt.colorbar()
+
+
+
+    plt.subplot(3, 2, 5)
+    plt.imshow(low_pass, cmap='gray')
+    plt.title('Low pass Filtering')
+
+    plt.show()
 
 
 
@@ -247,7 +305,8 @@ if __name__ == "__main__":
     #file_path = r"C:\Users\PRO\Desktop\Programy-Projekty\Python\pngs\poli.png"
     #read_png_header(file_path)
    # show_png(file_path)
-    furier_trans_png(file_path)
+    #furier_trans_png(file_path)
+    furier_trans_pngg(file_path, 20)
     #equals(file_path)
     metadata = read_png_metadata(file_path)
     if metadata:
